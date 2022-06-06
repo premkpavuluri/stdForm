393bf4bdd933599e61620f8231e3989ddb9b3515
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-06-06 16:25:34 +0530

    Changed the dataStructure for questionsConfig

--- form.js
+++ form.js
@@ -3,11 +3,14 @@ process.stdin.setEncoding('utf8');
 class Questions {
   constructor(questions) {
     this.questionsList = questions;
+    this.qTitles = Object.keys(questions);
     this.index = 0;
+    this.answers = {};
   }
 
   currentQuestion() {
-    return this.questionsList[this.index];
+    const qTitle = this.qTitles[this.index];
+    return this.questionsList[qTitle]?.question;
   }
 
   nextQuestion() {
@@ -15,29 +18,50 @@ class Questions {
   }
 
   isQuestionsOver() {
-    return this.questionsList.length < this.index;
+    return this.qTitles.length <= this.index;
+  }
+
+  recordInput(input) {
+    const qTitle = this.qTitles[this.index];
+    this.answers[qTitle] = input;
+  }
+
+  getAnswers() {
+    return this.answers;
   }
 }
 
 const main = () => {
-  const qlist = ['Name', 'DOB', 'Hobbies'];
-  const questions = new Questions(qlist);
-  const userInput = [];
+  const questionsConfig = {
+    Name: {
+      question: 'Enter your name:'
+    },
+    DOB: {
+      question: 'Enter your DOB'
+    },
+    Hobbies: {
+      question: 'Enter your hobbies'
+    }
+  };
+
+  const questions = new Questions(questionsConfig);
 
   console.log(questions.currentQuestion());
-  questions.nextQuestion();
-
   process.stdin.on('data', (input) => {
-    console.log(questions.currentQuestion());
+    questions.recordInput(input);
     questions.nextQuestion();
-    userInput.push(input);
+
     if (questions.isQuestionsOver()) {
       process.stdin.emit('close');
       process.exit(0);
     }
+
+    console.log(questions.currentQuestion());
   });
 
-  process.stdin.on('close', () => console.log('Thank you', userInput));
+  process.stdin.on('close', () => {
+    console.log('Thank you', questions.getAnswers());
+  });
 };
 
 main();
