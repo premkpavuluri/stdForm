e10d96a9b4d72333ef20c25603f790deb4ddd2a9
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-06-06 15:24:28 +0530

    Implemented simple bio-data form.

---
+++ form.js
@@ -0,0 +1,42 @@
+process.stdin.setEncoding('utf8');
+
+class Questions {
+  constructor(questions) {
+    this.questionsList = questions;
+    this.index = 0;
+  }
+
+  currentQuestion() {
+    return this.questionsList[this.index];
+  }
+
+  nextQuestion() {
+    this.index++;
+  }
+
+  isQuestionsOver() {
+    return this.questionsList.length < this.index;
+  }
+}
+
+const main = () => {
+  const qlist = ['Name', 'DOB', 'Hobbies'];
+  const questions = new Questions(qlist);
+  const userInput = [];
+
+  console.log(questions.currentQuestion());
+  questions.nextQuestion();
+
+  process.stdin.on('data', (input) => {
+    console.log(questions.currentQuestion());
+    questions.nextQuestion();
+    userInput.push(input);
+    if (questions.isQuestionsOver()) {
+      process.exitCode = 0;
+    }
+  });
+
+  process.stdin.on('close', () => console.log('Thank you', userInput));
+};
+
+main();
